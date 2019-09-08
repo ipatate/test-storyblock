a2cd115799655d4aa01a147515d6ff1c9124f57f
Patrick Faramaz <faramazpat@gmail.com>
2019-09-08 17:52:47 +0200

    test editable content

--- src/components/post.js
+++ src/components/post.js
@@ -1,10 +1,84 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SbEditable from 'storyblok-react';
 // import Components from './components.js';
+import config from '../../gatsby-config';
+
+const loadStoryblokBridge = function(cb) {
+  let sbConfigs = config.plugins.filter(item => {
+    return item.resolve === 'gatsby-source-storyblok';
+  });
+  let sbConfig = sbConfigs.length > 0 ? sbConfigs[0] : {};
+  let script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = `//app.storyblok.com/f/storyblok-latest.js?t=${sbConfig.options.accessToken}`;
+  script.onload = cb;
+  document.getElementsByTagName('head')[0].appendChild(script);
+};
+
+const getParam = function(val) {
+  var result = '';
+  var tmp = [];
+
+  window.location.search
+    .substr(1)
+    .split('&')
+    .forEach(function(item) {
+      tmp = item.split('=');
+      if (tmp[0] === val) {
+        result = decodeURIComponent(tmp[1]);
+      }
+    });
+
+  return result;
+};
 
 const Post = props => {
   const { blok } = props;
   const { Title, block } = blok;
+
+  useEffect(
+    () =>
+      loadStoryblokBridge(() => {
+        initStoryblokEvents();
+      }),
+    []
+  );
+
+  const loadStory = payload => {
+    window.storyblok.get(
+      {
+        slug: getParam('path'),
+        version: 'draft'
+      },
+      data => {
+        this.setState({ story: data.story });
+        this.loadGlovalNavi(data.story.lang);
+      }
+    );
+  };
+
+  const initStoryblokEvents = () => {
+    loadStory({ storyId: getParam('path') });
+
+    let sb = window.storyblok;
+
+    sb.on(['change', 'published'], payload => {
+      loadStory(payload);
+    });
+
+    sb.on('input', payload => {
+      if (this.state.story && payload.story.id === this.state.story.id) {
+        payload.story.content = sb.addComments(payload.story.content, payload.story.id);
+        this.setState({ story: payload.story });
+      }
+    });
+
+    sb.pingEditor(() => {
+      if (sb.inEditor) {
+        sb.enterEditmode();
+      }
+    });
+  };
   return (
     <SbEditable content={blok}>
       <div>
@@ -35,6 +109,7 @@ const Post = props => {
               </SbEditable>
             );
           }
+          return null;
         })}
       </div>
     </SbEditable>
